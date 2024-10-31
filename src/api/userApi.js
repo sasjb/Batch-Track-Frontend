7f214815fee2b46aa4c1892ca8bf04cbf29a65ed
Niaz <niaz.ju.cse@gmail.com>
2024-11-01 03:06:08 +0600

    connected frontend with backend

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:5000'; // Update with your backend's base URL
 
-export const addUser = (userData) => axios.post(`${API_URL}/saveUser`, userData);
-export const deleteUser = (userId) => axios.delete(`${API_URL}/deleteUser/${userId}`);
-export const updateUser = (userId, updatedData) => axios.put(`${API_URL}/updateUser/${userId}`, updatedData);
-export const getUsers = () => axios.get(`${API_URL}/getAllUsers`);
+export const addUser = (userData) => axios.post(`${API_URL}/api/user/saveUser`, userData);
+export const deleteUser = (userId) => axios.delete(`${API_URL}/api/user/deleteUser/${userId}`);
+export const updateUser = (userId, updatedData) => axios.put(`${API_URL}/api/user/updateUser/${userId}`, updatedData);
+export const getUsers = () => axios.get(`${API_URL}/api/user/getAllUsers`);
